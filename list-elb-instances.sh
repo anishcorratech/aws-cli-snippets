f2013b22bfa8d51216a9ea8649ce427bec439026
Anish Asokan <anish.asokan@corra.com>
2018-02-12 02:15:49 +0530

    Removing comments

--- list-elb-instances.sh
+++ list-elb-instances.sh
@@ -3,6 +3,5 @@
 for region in `aws --profile=$1 ec2 describe-regions --output text | cut -f3`
 do
      echo -e "\nListing ELB's in region:'$region'..."
-     #aws ec2 describe-load-balancers --profile=$1 --region $region | jq '.Reservations[] | ( .Instances[] | {state: .State.Name, name: .KeyName, type: .InstanceType, key: .KeyName})'
      aws elb describe-load-balancers --profile=$1 --region $region | jq -r '.LoadBalancerDescriptions[].CanonicalHostedZoneName'
 done
